61ff66c346dfcb8c9dd298344d7ec70cc9a136b4
rosemary <m.j.pourmoghadam@gmail.com>
2022-04-24 21:39:12 +0430

    methods initViews and areValidInputs defined in AddWordFragment.kt. id recieved as an argument id=-1 then user is adding new word else user editing a word.button save listener set. in WordDetailsFragment.kt also argument id recieve from home fragment button listeners set,method initViews defined ,some UI changed in  fragment_home.xml and fragment_word_details.xml. material button added. borders added in round_corner_shape.xml and round_corner_shape2.xml

--- app/src/main/java/com/example/dictionary/AddWordFragment.kt
+++ app/src/main/java/com/example/dictionary/AddWordFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.dictionary.database.Word
 import com.example.dictionary.databinding.FragmentAddWordBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class AddWordFragment : Fragment() {
     lateinit var binding: FragmentAddWordBinding
+    val vmodel: MainViewModel by viewModels()
+    var wordID = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
+            wordID = it.getInt("id")
         }
     }
 
@@ -21,13 +28,77 @@ class AddWordFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding= FragmentAddWordBinding.inflate(inflater,container,false)
-        return  binding.root
+        binding = FragmentAddWordBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        if (wordID != -1) {
+            initViews()
+        }
 
+        binding.buttonSave.setOnClickListener {
+
+            if (areValidInputs()) {
+                val tempWord = Word(
+                    0, binding.editTextWord.text.toString(),
+                    binding.editTextMeaning.text.toString(),
+                    binding.editTextSynonym.text.toString(),
+                    binding.editTextExample.text.toString()
+                )
+
+                if (wordID != -1) {
+                    vmodel.updateWord(
+                        Word(
+                            wordID,
+                            tempWord.word,
+                            tempWord.meaning,
+                            tempWord.synonyms,
+                            tempWord.example
+                        )
+                    )
+                } else {
+                    vmodel.addWord(tempWord)
+                }
+                Toast.makeText(requireContext(), "کلمه با موفقیت ذخیره شد", Toast.LENGTH_SHORT)
+                    .show()
+                findNavController().navigate(R.id.action_addWordFragment_to_homeFragment)
+            }
+        }
+
+
+
+    }
+
+    private fun initViews() {
+        val word = vmodel.findWordByID(wordID)
+        binding.editTextWord.setText(word.word)
+        binding.editTextMeaning.setText(word.meaning)
+        binding.editTextSynonym.setText(word.synonyms)
+        binding.editTextExample.setText(word.example)
+    }
+
+    private fun areValidInputs(): Boolean {
+        if (binding.editTextWord.text.isNullOrBlank()) {
+            binding.editTextWord.error = "لطفا فیلد را پر کنید"
+            return false
+        }
+
+        if (binding.editTextMeaning.text.isNullOrBlank()) {
+            binding.editTextMeaning.error = "لطفا فیلد را پر کنید"
+            return false
+        }
+
+        if (binding.editTextExample.text.isNullOrBlank()) {
+            binding.editTextExample.error = "لطفا فیلد را پر کنید"
+            return false
+        }
+        if (binding.editTextSynonym.text.isNullOrBlank()) {
+            binding.editTextSynonym.error = "لطفا فیلد را پر کنید"
+            return false
+        }
+        return true
 
     }
 
--- app/src/main/java/com/example/dictionary/WordDetailsFragment.kt
+++ app/src/main/java/com/example/dictionary/WordDetailsFragment.kt
@@ -5,55 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.dictionary.databinding.FragmentWordDetailsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WordDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WordDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    lateinit var binding:com.example.dictionary.databinding.FragmentWordDetailsBinding
+    val vmodel: MainViewModel by viewModels()
+    var wordID=-1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        wordID=it.getInt("id")
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_word_details, container, false)
+    ): View {
+        binding= FragmentWordDetailsBinding.inflate(inflater,container,false)
+    return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WordDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WordDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+
+
+        binding.buttonEdit.setOnClickListener {
+        val action=WordDetailsFragmentDirections.actionWordDetailsFragmentToAddWordFragment(wordID)
+            findNavController().navigate(action)
+        }
+
+        binding.buttonDelete.setOnClickListener {
+            vmodel.deleteWord(wordID)
+            findNavController().navigate(R.id.action_wordDetailsFragment_to_homeFragment)
+        }
+    }
+
+    private fun initViews(){
+        val word=vmodel.findWordByID(wordID)
+        binding.textViewWord.text=word.word
+        binding.textViewMeaning.text=word.meaning
+        binding.textViewSynonyms.text=word.synonyms
+        binding.textViewExample.text=word.example
     }
 }
