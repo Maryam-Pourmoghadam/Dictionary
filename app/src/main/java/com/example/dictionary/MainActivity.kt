07490f8ffcc120f88a30c41600e35279e754b9c9
rosemary <m.j.pourmoghadam@gmail.com>
2022-04-25 15:45:21 +0430

    codes about splash screen added in MainActivity.kt. an imageView and textView added in activity_main.xml.new vector added in drawable

--- app/src/main/java/com/example/dictionary/MainActivity.kt
+++ app/src/main/java/com/example/dictionary/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.dictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //splash
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+        val imageView=findViewById<ImageView>(R.id.splash_img)
+        val welcomeTv=findViewById<TextView>(R.id.textView_welcome)
+        val frg_container=findViewById<View>(R.id.nav_host_fragment)
+        imageView.alpha=0f
+        welcomeTv.alpha=0f
+        imageView.animate().setDuration(1000).alpha(1f)
+        welcomeTv.animate().setDuration(1500).alpha(1f).setStartDelay(500L).withEndAction{
+            frg_container.visibility=View.VISIBLE
+            imageView.visibility=View.GONE
+            supportActionBar?.show()
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
+        }
+
     }
 }
